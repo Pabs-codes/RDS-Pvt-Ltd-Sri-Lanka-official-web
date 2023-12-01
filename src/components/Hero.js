65abb5cf54c5d272d917c528631c54ace7bd72b7
Pabasara Navod <PabsCodes@gmail.com>
2023-12-01 13:42:22 +0530

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,65 @@
+// const Hero = () => {
+//     return <section
+//     className="hero section"
+//     id="home"
+//     style={{backgroundImage: 'url(./media/bg-hero1.jpg)'}}
+
+//   >
+//    <h1 className="hero__title">
+//     Elevate Your Projects <br />
+//     With Quality Steel <br />
+//     And Dependable Solutions.
+// </h1>
+//     <button className="hero__button">Start Your Inquiry Now</button>
+//   </section>
+// }
+
+// export default Hero;
+
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+
+     const inquireNow = () => {
+       window.scrollTo({
+        top: 950,
+        behavior: 'smooth'
+       })
+     }
+
+
+
+  return (
+    <section className="hero section" id="home" style={{ position: 'relative', overflow: 'hidden' }}>
+      <video
+        className="hero-video"
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+        }}
+      >
+        <source src="./media/drone.mp4" type="video/mp4" />
+        {/* You can add additional source elements for different video formats (e.g., webm, ogg) */}
+        Your browser does not support the video tag.
+      </video>
+      <div className="hero__content" style={{ zIndex: 1, color: '#fff', textAlign: 'center', position: 'relative' }}>
+      <h1 className="hero__title animated-text">
+        Elevate Your Projects <br />
+        With Quality Steel <br />
+        And Dependable Solutions.
+      </h1>
+      <button className="hero__button animated-button on" onClick={inquireNow}>Start Your Inquiry Now</button>
+   </div>
+    </section>
+  );
+}
+
+export default Hero;
+
