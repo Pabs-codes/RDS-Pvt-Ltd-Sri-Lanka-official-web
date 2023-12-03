6527b2487d58ad955941b635698c687aa243ffc3
Pabasara Navod <PabsCodes@gmail.com>
2023-12-03 12:39:16 +0530

    upp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import Products from './components/Products';
-import Place from './components/Place';
-import Projects from './components/Projects';
-import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Hero from './components/Hero';
+// import Services from './components/Services';
+// import Products from './components/Products';
+// import Place from './components/Place';
+// import Projects from './components/Projects';
+// import Footer from './components/Footer';
 import Error from './components/Error';
 
 
